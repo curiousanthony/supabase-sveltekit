e8617c75a46499bbd63dd45c41f46cf4b3a3fb3c
curiousanthony <195097839+curiousanthony@users.noreply.github.com>
2025-10-24 17:03:58 +0200

    rollback: reverted ssl require change & deploying after editing db url in .env for prod

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -3,8 +3,5 @@ import postgres from 'postgres'
 import * as schema from './schema'
 import { DATABASE_URL } from '$env/static/private';
 
-const client = postgres(DATABASE_URL!, {
-    ssl: 'require',
-    max: 10
-})
+const client = postgres(DATABASE_URL!);
 export const db = drizzle(client, { schema });
