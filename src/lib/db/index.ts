19844763c756d4a241d7700b8fe36565f4403263
curiousanthony <195097839+curiousanthony@users.noreply.github.com>
2025-10-24 16:50:58 +0200

    debug: ssl require in db index.ts to try to fix prod db bug

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -3,5 +3,8 @@ import postgres from 'postgres'
 import * as schema from './schema'
 import { DATABASE_URL } from '$env/static/private';
 
-const client = postgres(DATABASE_URL)
+const client = postgres(DATABASE_URL!, {
+    ssl: 'require',
+    max: 10
+})
 export const db = drizzle(client, { schema });
