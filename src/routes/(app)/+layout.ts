0f34c9a2a9e6ddd71ad32668a5537de040d142cd
curiousanthony <195097839+curiousanthony@users.noreply.github.com>
2025-10-29 17:39:36 +0100

    fix: added startsWith instead of substring matching for page name in layout.ts

--- src/routes/(app)/+layout.ts
+++ src/routes/(app)/+layout.ts
@@ -3,6 +3,8 @@ import type { LayoutLoad } from './$types';
 
 export const load: LayoutLoad = ({ url }) => {
   // const pageName = sitemap.find((item) => item.url === url.pathname)?.title ?? 'Default Page name';
-  const pageName = sitemap.find((item) => url.pathname.includes(item.url))?.title ?? 'Default Page name';
+  const pageName = [...sitemap]
+    .sort((a, b) => b.url.length - a.url.length)
+    .find((item) => url.pathname.startsWith(item.url))?.title ?? 'Default Page name';
   return { pageName };
 };
