d368b462f5d9c5c7841638deba78849df635b7a8
curiousanthony <195097839+curiousanthony@users.noreply.github.com>
2025-11-05 00:30:05 +0100

    feat: working command palette for navigation

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -2,12 +2,16 @@
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import AppSidebar from '$lib/components/app-sidebar.svelte';
 	import SiteHeader from '$lib/components/site-header.svelte';
-	// import { sitemap } from '$lib/settings/config';
+	import * as Command from '$lib/components/ui/command/index.js';
+	import { sitemap } from '$lib/settings/config';
 	// import { page } from '$app/state';
 
 	import { ModeWatcher } from 'mode-watcher';
 	// import type { LayoutProps } from '../$types';
 	import { page } from '$app/state';
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
+	import { Kbd } from '$lib/components/ui/kbd/index.js';
+	import { IconArrowBack, IconSettings } from '@tabler/icons-svelte';
 
 	let { data, children } = $props();
 
@@ -24,7 +28,29 @@
 	// Test for headerActions based on page.data
 	// const headeractions = $derived(page.data?.headerActions ?? null);
 	const header = $derived(page.data?.header ?? null);
+
+	// Command palette
+	let open = $state(false);
+
+	function handleKeydown(e: KeyboardEvent) {
+		if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+			e.preventDefault();
+			open = !open;
+		}
+
+		// if (e.key === 'Escape') {
+		// 	e.preventDefault();
+		// 	open = false;
+		// }
+		// Close the command palette when pressing Return (Enter)
+		if (e.key === 'Enter') {
+			e.preventDefault();
+			open = false;
+		}
+	}
 </script>
+
+<svelte:document onkeydown={handleKeydown} />
 
 <svelte:head>
 	<title>{header?.pageName ?? pageTitle}</title>
@@ -48,3 +74,36 @@
 		</div>
 	</main>
 </Sidebar.Provider>
+
+<Command.Dialog bind:open>
+	<Command.Input placeholder="Tapez où vous souhaitez naviguer..." />
+	<Command.List>
+		<Command.Empty>Aucun résultat trouvé.</Command.Empty>
+		<Command.Group heading="Principal">
+			<!-- <Command.LinkItem href="/formations">Formations</Command.LinkItem> -->
+			{#each sitemap as item (item.url)}
+				<!-- Render below link item if url pathname is NOT the same as the current page's pathname-->
+				{#if item.url !== page.url.pathname}
+					<Command.LinkItem href={item.url}>
+						<item.icon />
+						{item.title}
+					</Command.LinkItem>
+				{/if}
+			{/each}
+		</Command.Group>
+		<Command.Separator />
+		<Command.Group heading="Autre">
+			<Command.LinkItem href="/parametres">
+				<IconSettings />
+				Paramètres
+			</Command.LinkItem>
+		</Command.Group>
+	</Command.List>
+	<!-- <span>Footer</span> -->
+	<div class="flex items-center gap-2 bg-muted/60 px-4 py-3">
+		<Kbd>
+			<IconArrowBack class="size-4" />
+		</Kbd>
+		<span class="text-sm text-muted-foreground">Aller à la page</span>
+	</div>
+</Command.Dialog>
